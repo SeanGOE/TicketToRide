1c754fab7a3b338d4ace639047193239928e96f2
SeanGOE <gabsean12@gmail.com>
2023-05-25 12:44:18 -0700

    asjkldf;ljksfda

---
+++ L2_adjacency_list_graph.py
@@ -0,0 +1,63 @@
+from typing import Set, List, Dict
+from graph_base_class import Edge, GraphBaseClass
+
+
+# Implement Graph using an adjacency list as the underlying representation
+class AdjacencyListGraph(GraphBaseClass):
+
+    # if is_directed is true, this should be a directed graph.  If false, it's an undirected graph
+    # Make sure your implementation accounts for node order on an edge for directed and is neutral for undirected
+    # super sets a property self.is_directed to the parameter value
+    def __init__(self, is_directed:bool) -> None:
+        self.nodes:Dict[Set] = dict()
+        super().__init__(is_directed)
+    
+    # Adds a node named "name" to the graph.
+    def add_node(self, name:any) -> None:
+        self.nodes.setdefault(name, set())
+
+    # Removes the node named "name" and all arcs connected to it
+    def remove_node(self, name:any) -> None:
+        self.nodes.pop(name)
+        for key in self.nodes:
+            dumb_list = []
+            for set in self.nodes[key]:
+                if set.finish == name:
+                    dumb_list.append(set)
+            for item in dumb_list:
+                self.nodes[key].remove(item)
+                    
+    
+    # Gets a set of the names of all nodes in the graph
+    def get_nodes(self) -> Set[any]:
+        all_nodes = set()
+        for key in self.nodes:
+            all_nodes.add(key)
+        return all_nodes
+
+
+    # Returns True if the node name1 is connected to the node name2 and False otherwise
+    def is_connected(self, name1:any, name2:any) -> None:
+        for items in self.nodes[name1]:
+            if items.finish == name2:
+                return True
+        return False
+
+    # Adds an edge from node start to node finish with the weight specified
+    def add_edge(self, start:any, finish:any, weight:int) -> None:
+        new_edge = Edge(start, finish, weight)
+        self.nodes[start].add(new_edge)
+
+    # Returns a set of the names of nodes adjacent to the given node (i.e. there's an arc from the node to the neighbor)
+    def get_neighbors(self, name:str) -> Set[any]:
+        all_neighbors = set()
+        for thing in self.nodes[name]:
+            all_neighbors.add(thing.finish)
+        return all_neighbors
+    
+    # Gets a set of edges leading out of the given node
+    def get_edges(self, name:str) -> Set[Edge]:
+        all_edges = set()
+        for thing in self.nodes[name]:
+            all_edges.add(thing)
+        return all_edges
--- path_finding.py
+++ path_finding.py
@@ -2,6 +2,8 @@ from math import sqrt
 import sys
 from typing import Dict, List, Set, Tuple
 from priority_queue import PriorityQueue
+import L2_adjacency_list_graph
+from L2_adjacency_list_graph import AdjacencyListGraph
 
 # You do not need to change this class.  It is used as the return type for get_minimum_path
 class RouteInfo:
@@ -23,28 +25,36 @@ class Edge:
 # of your choice. Feel free to use your graph classes from the practice exercises; copy the appropriate
 # files into your project and import the classes at the top of this file.
 class PathFinder:
-    def __init__(self) -> None:
-        self.nodes:Dict[Set] = dict()
+    def __init__(self, is_directed = True) -> None:
+        self.graph = AdjacencyListGraph(is_directed)
+        self.ids_to_name = {}
+        self.name_to_ids = {}
 
     # TODO: adds an edge to the graph, using a the id of the start node and id of the finish node
     def add_edge(self, start_id: int, finish_id:int , cost: float) -> None:
-        new_edge = Edge(start_id, finish_id, cost)
-        self.nodes[start_id].add(new_edge)
+        self.graph.add_edge(start_id, finish_id, cost)
 
     # TODO: adds a node to the graph, passing in the id, friendly name, and location of the node.
     # location is a tuple with the x and y coordinates of the location
     def add_node(self, id: int, name: str, location: Tuple[float, float]) -> None:
-        self.nodes.setdefault(name, set())
+        self.graph.add_node(id)
+        self.ids_to_name[id] = (name, location)
+        self.name_to_ids[name] = (id, location)
 
     # TODO: calculates the minimum path using the id of the start city and id of the destination city, using A*
     # Returns a RouteInfo object that contains the edges for the route.  See RouteInfo above for attributes
     # Note: This implementation should use A*.  Tests that should pass 
     def get_minimum_path(self, start_city_id: int, destination_id:int ) -> RouteInfo:
-        pri_queue = PriorityQueue()
-        for edge in self.nodes:
-            pass
-        start = RouteInfo()
-        pri_queue.enqueue()
+        visited = {}
 
-    def get_min_path_recursive(self, location, destination):
-        pass
+        pri_queue = PriorityQueue()
+
+        start = RouteInfo([self.ids_to_name[start_city_id]], [start_city_id], 0)
+
+        pri_queue.enqueue(0, start)
+        while pri_queue.peek() != destination_id:
+            item = pri_queue.heap[1]
+            for nodes in self.graph.nodes[item.value]:
+                pri = nodes.weight + item.priority
+                val = nodes.finish
+
